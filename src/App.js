280c3d8148ec7e8676888bb99e3ae624caca7cb6
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2021-07-29 00:03:13 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Product from './components/Pages/Product';
 import Welcome from './components/Pages/Welcome'
 import MainHeader from './components/MainHeader';
+import ProductDetail from './components/Pages/ProductDetail';
 
 function App() {
   return (
     <div>
-      <MainHeader/>
-      <Route path="/welcome">
-        <Welcome/>
-      </Route>
-      <Route path="/product">
-        <Product/>
-      </Route>
+        <MainHeader/>
+        <main> 
+          <Switch>
+          <Route path="/welcome">
+            <Welcome/>
+          </Route>
+          <Route path="/product">
+            <Product/>
+          </Route>
+          <Route path="/product/:productId">
+            <ProductDetail/>
+          </Route>
+          </Switch>
+        </main>
+    
     </div>
   );
 }
--- src/components/Pages/Product.js
+++ src/components/Pages/Product.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Product = () =>{
   return (
   <section>
     <h1>The Product page.</h1>  
+    <ul>
+      <li><Link to="/product/:1">Book 1</Link> </li>
+      <li><Link to="/product/:2">Carpet 2</Link></li>
+      <li><Link to="/product/:3">An Online Course 3 </Link></li>
+    </ul>
   </section>);
 }
 
